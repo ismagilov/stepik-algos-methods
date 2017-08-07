181bf40595b058e98ec4cc6040a348e3b25cdc27
Ilya Ismagilov <ismagilov@gmail.com>
2017-08-07 16:19:36 +0700

    Solved Fibonacci last digit

---
+++ src/fibonacci_last_digit/Main.java
@@ -0,0 +1,34 @@
+package fibonacci_last_digit;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int lastFibDigit(int nth) {
+        int ldpp = 0;
+        int ldp = 1;
+
+        if (nth == 0)
+            return ldpp;
+        else if (nth == 1)
+            return ldp;
+
+        int ldn = 0;
+        for (int i = 2; i <= nth; i++) {
+            ldn = (ldpp + ldp) % 10;
+
+            ldpp = ldp;
+            ldp = ldn;
+        }
+
+        return ldn;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int nth = s.nextInt();
+
+        System.out.println(new Main().lastFibDigit(nth));
+    }
+}
